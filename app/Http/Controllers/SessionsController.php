0640c16088d420685c2a368d82504e471dc6e878
huaiguoguo <crazywork@foxmail.com>
2023-08-27 18:47:57 +0800

    用户激活

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -16,7 +16,7 @@ class SessionsController extends Controller
     public function __construct()
     {
         $this->middleware('guest', [
-            'only'=>['create']
+            'only' => ['create']
         ]);
     }
 
@@ -41,10 +41,16 @@ class SessionsController extends Controller
         ]);
 
         if (Auth::attempt($credentials, $request->has('remember'))) {
-            session()->flash('success', '欢迎回来！');
-            $fallback = route('users.show', Auth::user());
-            return redirect()->intended($fallback);
-            // return redirect()->route('users.show', [Auth::user()]);
+            if (Auth::user()->activated) {
+                session()->flash('success', '欢迎回来！');
+                $fallback = route('users.show', Auth::user());
+                return redirect()->intended($fallback);
+                // return redirect()->route('users.show', [Auth::user()]);
+            }else{
+                Auth::logout();
+                session()->flash('warning', '您的账号未激活, 请检查邮箱中的注册邮件进行激活.');
+                return redirect('/');
+            }
         } else {
             session()->flash('danger', '很抱歉，您的邮箱和密码不匹配');
             return redirect()->back()->withInput();
