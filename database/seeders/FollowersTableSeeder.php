573a4f818441cc1791794737b44c28521414bf35
huaiguoguo <crazywork@foxmail.com>
2023-09-08 20:08:25 +0800

    社交统计信息

---
+++ database/seeders/FollowersTableSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class FollowersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $users = User::all();
+        $user = $users->first();
+        $user_id = $user->id;
+
+        $followers = $users->slice(1);
+        $follower_ids = $followers->pluck('id')->toArray();
+
+        //关注除了1号用户以外的所有用户
+        $user->follow($follower_ids);
+        foreach ($followers as $key=>$follower){
+            $follower->follow($user_id);
+        }
+
+    }
+}
